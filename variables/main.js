98c4e4ce0babf61a60109465ab869226adb00ef1
shpulwind <shpulvind@ya.ru>
2019-09-23 12:47:04 +0300

    Homework 1.1 done

---
+++ variables/main.js
@@ -0,0 +1,20 @@
+// Задача 1
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let averageMark = ((algebra + geography + physics) / 3);
+console.log(averageMark);
+
+// Задача 2
+let name = 'Саша';
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+// Задача 3
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
