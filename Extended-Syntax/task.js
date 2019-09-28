baeff30dd74d0d4dee5c9626fcbe6a7b9b5658be
shpulwind <shpulvind@ya.ru>
2019-09-28 17:28:54 +0300

    Homework Extended-Syntax completed

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -11,8 +11,18 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    // Задача №1
+    "use strict";
+    let x = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d > 0) {
+    x[0] = (-b + Math.sqrt(d)) / (2 * a);
+    x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    } else if (d == 0) {
+    x[0] = (-b + Math.sqrt(d)) / (2 * a);
+    } else {
+    }
+    return x;
 }
 
 function calculateAverageRating(){
@@ -22,8 +32,17 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+    // Задача №2
+    if (marks.length > 5) {
+        console.log("Оценок много. Берём первые пять");
+        marks = marks.slice(0, 5);
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum = sum + marks[i];
+    }
+    let averageMark = sum / marks.length;
+    return averageMark;
 }
 
 function calculateDrinkTask(){
@@ -34,7 +53,15 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    // Задача №3
+    let currentYear = new Date()
+    let visitorAge = currentYear.getFullYear() - dateOfBirthday.getFullYear();
+    let result
+    if (visitorAge < 18) {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    } else {
+        result = `Не желаете ли олд-фэшн, ${name}`;
+    }
+    console.log(result);
+    return result;
 }
