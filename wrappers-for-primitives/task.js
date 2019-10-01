40a0973ade255df9ecfdd9c8e1fa1e03961593b4
shpulwind <shpulvind@ya.ru>
2019-10-01 13:38:20 +0300

    Homework Wrappers for primitives done

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -9,10 +11,37 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
+// Задача №1
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    for (let i = 0; i < 3; i++) {
+        if ((typeof arguments[i] != "number"
+        && typeof Number(arguments[i]) != "number")
+        || isNaN(Number(arguments[i]))) {
+            let argumentName;
+            switch (i) {
+                case 0:
+                    argumentName = "Процентная ставка";
+                    break;
+                case 1:
+                    argumentName = "Начальный взнос";
+                    break;
+                case 2:
+                    argumentName = "Общая стоимость";
+                    break;
+            }
+            return `Параметр \"${argumentName}\" содержит неправильное значение \"${arguments[i]}\"`;
+        }
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    percent /= 100;
+    let p = percent / 12;
+    let nowDate = new Date();
+    let fullTimeMortgage = Math.round((Date.parse(date) - Date.parse(nowDate)) / 1000 / 60 / 60 / 24 / 30);
+    let creditSum = amount - contribution;
+    let monthlyPayment = creditSum * (p + p / ((1 + p) ** fullTimeMortgage - 1));
+    let totalAmount = monthlyPayment * fullTimeMortgage;
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -22,7 +51,11 @@ function sayHello() {
     span.textContent = greeting;
 }
 
+// Задача №2
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (isNaN(name)) {
+        return `Привет, мир! Меня зовут ${name}`;
+      } else {
+        return `Привет, мир! Меня зовут Аноним`;
+    }
 }
